047ca56c8704d24a8fbc5b37877d847f172a51e0
dailibo <dailibo@wisdytech.cn>
2019-05-04 23:12:39 +0800

    first commit

---
+++ src/main/java/com/wisdytech/extjs/ExtjsApplication.java
@@ -0,0 +1,13 @@
+package com.wisdytech.extjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtjsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wisdytech/extjs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wisdytech.extjs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExtjsApplication.class);
+    }
+
+}
